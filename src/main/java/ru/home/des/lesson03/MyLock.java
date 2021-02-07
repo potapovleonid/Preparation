675a34985572545bfe839baa5c6877ddec17d583
Leonid <kissmykrabeskill@gmail.com>
2021-02-07 17:41:14 +0500

    Add Lesson_03

---
+++ src/main/java/ru/home/des/lesson03/MyLock.java
@@ -0,0 +1,40 @@
+package ru.home.des.lesson03;
+
+import com.sun.corba.se.impl.orbutil.concurrent.Sync;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyLock implements Lock {
+    private final Sync sync;
+
+    public MyLock(Sync sync) {
+        
+    }
+
+    public void lock() {
+
+    }
+
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    public boolean tryLock() {
+        return false;
+    }
+
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    public void unlock() {
+
+    }
+
+    public Condition newCondition() {
+        return null;
+    }
+}
