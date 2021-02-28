1cf7ec1308744dc4e122a2a1ed27f9a06cce40de
Leonid <kissmykrabeskill@gmail.com>
2021-02-28 14:57:19 +0500

    fix

--- lessson07/src/main/java/ru/home/des/repositories/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package ru.home.des.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.home.des.entity.Student;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-
-}
